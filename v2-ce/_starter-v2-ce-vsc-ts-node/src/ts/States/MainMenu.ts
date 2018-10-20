8378587b812da3c80ead9f69d5bfe110b3f4a6e0
James Skemp <strivinglife@gmail.com>
2018-10-20 15:15:13 -0500

    Update Phaser CE starter to include Phaser logo

--- v2-ce/_starter-v2-ce-vsc-ts-node/src/ts/States/MainMenu.ts
+++ v2-ce/_starter-v2-ce-vsc-ts-node/src/ts/States/MainMenu.ts
@@ -4,9 +4,15 @@
 	 */
 	public static Name: string = "MainMenu";
 
+	preload() {
+		this.load.path = "assets/";
+		this.load.image("Phaser-Logo-Small");
+	}
+
 	create() {
 		console.log((new Date).toISOString() + ' : Entered MainMenu create()');
 
+		this.add.image(this.game.world.centerX, this.game.world.centerY, "Phaser-Logo-Small").anchor.setTo(0.5);
 		// Handle user input as needed.
 	}
 }
